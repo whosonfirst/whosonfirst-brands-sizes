02118b85831fa7a5b1582be1f94a70779d8bc1fd
thisisaaronland <whosonfirst@mapzen.com>
2017-11-27 23:12:25 +0000

    size not name; rebuild specs

--- bin/compile.py
+++ bin/compile.py
@@ -14,15 +14,15 @@ if __name__ == '__main__':
     bin = os.path.dirname(whoami)
     root = os.path.dirname(bin)
 
-    sizes = os.path.join(root, 'sizes')
+    source = os.path.join(root, 'sizes')
     spec = {}
 
     labels = {}
-    names = {}
+    sizes = {}
 
-    required = ("id", "name", "label")
+    required = ("id", "size", "label")
 
-    for (root, dirs, files) in os.walk(sizes):
+    for (root, dirs, files) in os.walk(source):
 
         for f in files:
     
@@ -43,16 +43,16 @@ if __name__ == '__main__':
                     logging.error("%s is missing a %s key" % (path, k))
                     sys.exit()
 
-            name = data['name']
+            size = data['size']
             label = data['label']
 
-            if name in names:
-                logging.error("%s is trying to claim name '%s' (already assigned to %s)" % (path, name, names[name]))
+            if size in sizes:
+                logging.error("%s is trying to claim size '%s' (already assigned to %s)" % (path, size, sizes[size]))
 
             if label in labels:
-                logging.error("%s is trying to claim name '%s' (already assigned to %s)" % (path, label, labels[label]))
+                logging.error("%s is trying to claim size '%s' (already assigned to %s)" % (path, label, labels[label]))
 
-            names[ name ] = path
+            sizes[ size ] = path
             labels[ label ] = path
 
             spec[data['id']] = data
